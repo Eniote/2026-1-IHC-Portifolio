510b2cfeafeecfa0d94449f47057afa09901b10d
Lucas Coutinho <lucascoutinhocunha@gmail.com>
2026-05-17 22:56:09 -0300

    personas

--- docs/script.js
+++ docs/script.js
@@ -1,11 +1,17 @@
-let ids = 6
+let ids = 10
 
 
 function hideAll()
 {
-    for (let i = 1; i<(ids+1); i++){
+    for (let i = 0; i<ids; i++){
         document.getElementById("A" + i).hidden = true
     }
+}
+
+function A0()
+{
+    hideAll()
+    document.getElementById("A0").hidden = false
 }
 
 function A1()
@@ -43,6 +49,25 @@ function A6()
     hideAll()
     document.getElementById("A6").hidden = false
 }
+
+function A7()
+{
+    hideAll()
+    document.getElementById("A7").hidden = false
+}
+
+function A8()
+{
+    hideAll()
+    document.getElementById("A8").hidden = false
+}
+
+function A9()
+{
+    hideAll()
+    document.getElementById("A9").hidden = false
+}
+
 /*
 function reveal(A)
 {
